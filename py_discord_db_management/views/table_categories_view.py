7a3d3e17084a13e69a57c694d33dfe04f9d6963b
Cedric <111773375+cedriclmao@users.noreply.github.com>
2023-06-08 02:11:57 +0200

    Update table_categories_view.py
    cleaning

--- py_discord_db_management/views/table_categories_view.py
+++ py_discord_db_management/views/table_categories_view.py
@@ -8,6 +8,7 @@ class TableCategoryView(discord.ui.View):
         self.add_item(self.AddDataButton(database, table))
         self.add_item(self.DeleteDataButton(database, table))
 
+        
     class ReturnOverviewButton(discord.ui.Button):
         def __init__(self, database):
             super().__init__(label="Return")
@@ -18,6 +19,8 @@ class TableCategoryView(discord.ui.View):
 
             from py_discord_db_management.views.table_overview_view import TableOverviewView
             await interaction.message.edit(view=TableOverviewView(database=self.database))
+            
+            
     class AddDataButton(discord.ui.Button):
         def __init__(self, database, table):
             super().__init__(label="Add Data", style=discord.ButtonStyle.green)
@@ -33,6 +36,7 @@ class TableCategoryView(discord.ui.View):
             from py_discord_db_management.views.table_add_data_view import TableAddDataView
             await interaction.message.edit(view=TableAddDataView(database=self.database, table=self.table, columns=columns))
 
+            
     class DeleteDataButton(discord.ui.Button):
         def __init__(self, database, table):
             super().__init__(label="Delete Data", style=discord.ButtonStyle.danger)
